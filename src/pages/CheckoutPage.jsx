c5427a3a16d725c048eaac52b597418617e70c2f
amirkhoda8 <74904393+amirkhoda@users.noreply.github.com>
2024-03-11 01:15:35 +0330

    add checkout page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useCart } from "../context/CartContext";
+import BasketCard from "../components/BasketCard";
 
 function Checkout() {
-  return <div>Checkout</div>;
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => {
+    dispatch({ type, payload });
+  };
+  return (
+    <div>
+      <div>
+        {state.selectedItems.map((product) => (
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
